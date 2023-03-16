c7677ca66e2467b3f90b59a5fd2f6503cdbac4d3
Oluid07 <oluidclay@hotmail.com>
2023-03-16 16:47:45 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - main is the entry point of the program
+ *
+ *Return: returns zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
