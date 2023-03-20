9889fd7641d7c834257463ea020008fc66e61786
Oluid07 <oluidclay@hotmail.com>
2023-03-20 13:36:20 -0700

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main is the entry point to the program
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
