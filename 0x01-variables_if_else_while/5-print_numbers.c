05ac479128cae1fe603d8f7d880fb88964197620
Oluid07 <oluidclay@hotmail.com>
2023-03-20 14:56:01 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main is the entry point of the program
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+		printf("%d", i);
+	return (0);
+}
