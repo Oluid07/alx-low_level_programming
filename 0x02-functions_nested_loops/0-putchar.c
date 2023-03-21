1fbc389f6f0b9e9a29b2acdef63560765c577384
Oluid07 <oluidclay@hotmail.com>
2023-03-21 12:01:34 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - main is the entry point of the program
+ * _putchar: prints an alphabet or a character
+ * Return: retrns zero
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
