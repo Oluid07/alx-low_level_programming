7c0afdaf33021124b32162db1cdd3d61a46c2071
Oluid07 <oluidclay@hotmail.com>
2023-03-22 14:31:44 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds the integers
+ *
+ * @x: defines the x parameter
+ * @y: defines the y parameter
+ * Return: returns sum
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
