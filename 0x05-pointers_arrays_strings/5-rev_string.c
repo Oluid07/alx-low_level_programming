115ab7c65648fbde4a4556d422068453fad3c98d
Oluid07 <oluidclay@hotmail.com>
2023-03-28 13:30:04 -0700

    good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - prints strings in reverse
+ *
+ * @s: defines the parameter
+ * Return: returns zero
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int count = 0;
+	int i;
+
+	while (s[count] != '\0')
+		count++;
+	for (i = 0; i < count; i++)
+	{
+		count--;
+		rev = s[i];
+		s[i] = s[count];
+		s[count] = rev;
+	}
+}
